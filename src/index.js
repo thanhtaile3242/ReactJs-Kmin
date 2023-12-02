f80bbdcc74c7d5714ea6766e9d18ddc7cd58e9e0
lethanhtai <thanhtaile3242@gmail.com>
2023-12-02 15:15:36 +0700

    2/12

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
-
+let color = "yellow";
+const color2 = "green";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+const changeBackgroundColor = () => {
+    color = color === "yellow" ? "green" : "yellow";
+    renderApp();
+};
+
+const renderApp = () => {
+    const h1 = (
+        <>
+            <h1
+                style={{ color: "red", backgroundColor: color }}
+                onClick={() => {
+                    changeBackgroundColor();
+                }}
+            >
+                Hellooooooo
+            </h1>
+            ;
+        </>
+    );
+
+    root.render(h1);
+};
+renderApp();
+// root.render(
+//     // <React.StrictMode>
+//     <App />
+//     // </React.StrictMode>
+// );
