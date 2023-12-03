626b7e96bd6ee47a788716814f04865317189d07
lethanhtai <thanhtaile3242@gmail.com>
2023-12-03 13:23:19 +0700

    3/12

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,14 @@
+import React from "react";
+class Counter extends React.Component {
+    render() {
+        return (
+            <>
+                <div>
+                    <h1>Lê Thành Tài</h1>
+                </div>
+            </>
+        );
+    }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.js";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-let color = "yellow";
-//
-const changeBackgroundColor = () => {
-    color = color === "yellow" ? "green" : "yellow";
-    renderApp();
-};
-//
-const renderApp = () => {
-    const h1 = (
-        <>
-            <h1
-                style={{ color: "red", backgroundColor: color }}
-                onClick={() => {
-                    changeBackgroundColor();
-                }}
-            >
-                Hellooooooo
-            </h1>
-        </>
-    );
+import Counter from "./components/Counter/Counter.js";
+// import App from "./App.js";
 
-    root.render(h1);
-};
-renderApp();
-// root.render(
-//     // <React.StrictMode>
-//     <App />
-//     // </React.StrictMode>
-// );
+// let color = "yellow";
+// //
+// const changeBackgroundColor = () => {
+//     color = color === "yellow" ? "green" : "yellow";
+//     renderApp();
+// };
+// //
+// const renderApp = () => {
+//     const h1 = (
+//         <>
+//             <h1
+//                 style={{ color: "red", backgroundColor: color }}
+//                 onClick={() => {
+//                     changeBackgroundColor();
+//                 }}
+//             >
+//                 Hellooooooo
+//             </h1>
+//         </>
+//     );
+
+//     root.render(h1);
+// };
+// renderApp();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    // <React.StrictMode>
+    <Counter />
+    // </React.StrictMode>
+);
