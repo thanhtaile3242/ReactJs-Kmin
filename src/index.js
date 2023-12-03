a98553af6df6730158bb4d74a082d5f16ed1cb87
lethanhtai <thanhtaile3242@gmail.com>
2023-12-03 13:24:43 +0700

    aaa

--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Counter from "./components/Counter/Counter.js";
-// import App from "./App.js";
-
-// let color = "yellow";
-// //
-// const changeBackgroundColor = () => {
-//     color = color === "yellow" ? "green" : "yellow";
-//     renderApp();
-// };
-// //
-// const renderApp = () => {
-//     const h1 = (
-//         <>
-//             <h1
-//                 style={{ color: "red", backgroundColor: color }}
-//                 onClick={() => {
-//                     changeBackgroundColor();
-//                 }}
-//             >
-//                 Hellooooooo
-//             </h1>
-//         </>
-//     );
-
-//     root.render(h1);
-// };
-// renderApp();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
