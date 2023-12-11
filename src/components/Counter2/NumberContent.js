2485ce4a750f26befcaf9ae04c5f3b7f80d7cbf5
lethanhtai <thanhtaile3242@gmail.com>
2023-12-11 11:01:41 +0700

    E02 version2

--- src/components/Counter2/NumberContent.js
+++ src/components/Counter2/NumberContent.js
@@ -1,37 +1,15 @@
 import "./Num.scss";
 import { useState } from "react";
 const NumberContent = (props) => {
-    // const [value, setValue] = useState(props.number);
     const value = props.number;
-    // const handleChange = (type) => {
-    //     if (type === "DOWN") {
-    //         setValue(value - 1);
-    //     }
-    //     if (type === "UP") {
-    //         setValue(value + 1);
-    //     }
-    // };
+
     console.log(value);
     return (
         <>
             <div style={{ fontWeight: "bold" }}>
-                <span
-                    className="btn-change"
-                    // onClick={() => {
-                    //     handleChange("DOWN");
-                    // }}
-                >
-                    &lt;
-                </span>
+                <span className="btn-change">&lt;</span>
                 <span>{value}</span>
-                <span
-                    className="btn-change"
-                    // onClick={() => {
-                    //     handleChange("UP");
-                    // }}
-                >
-                    &gt;
-                </span>
+                <span className="btn-change">&gt;</span>
             </div>
         </>
     );
